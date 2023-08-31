2db62c974b02848b7e8cee161a438c95fbc3b0ec
SHUBHAM AWASYA <63654949+SubhamAwasya@users.noreply.github.com>
2023-08-31 19:58:48 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import { useState } from "react";
+import Form from "./components/form";
+function App() {
+  return (
+    <>
+      <Form />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/form.jsx
@@ -0,0 +1,70 @@
+import React, { useState } from "react";
+
+function Form() {
+  const [isRegisterd, setIsRefisterd] = useState(false);
+
+  return (
+    <>
+      <form>
+        <h2>
+          {isRegisterd ? "Enter detail to register" : "Enter detail to login"}
+        </h2>
+        {isRegisterd && (
+          <input
+            className="finput"
+            name="fname"
+            placeholder="First Name"
+            type="text"
+            required
+          ></input>
+        )}
+        {isRegisterd && (
+          <input
+            className="finput"
+            name="lname"
+            placeholder="Last Name"
+            type="text"
+            required
+          ></input>
+        )}
+        <input
+          className="finput"
+          name="email"
+          placeholder="Email"
+          type="email"
+          required
+        ></input>
+        <input
+          className="finput"
+          name="password"
+          placeholder="Password"
+          type="password"
+          required
+        ></input>
+        {isRegisterd && (
+          <input
+            className="finput"
+            name="confirmpassword"
+            placeholder="Confirm Password"
+            type="password"
+            required
+          ></input>
+        )}
+
+        <button className="sbtn" type="submit">
+          {isRegisterd ? "Sign up" : "Log in"}
+        </button>
+        <a
+          onClick={() => {
+            {
+              setIsRefisterd(!isRegisterd);
+            }
+          }}
+        >
+          {isRegisterd ? "Alredy a user log in" : "Register a new user"}
+        </a>
+      </form>
+    </>
+  );
+}
+export default Form;
