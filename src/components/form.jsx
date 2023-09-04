1bdd77906fd05e345a8ee74f407649d44f309282
SHUBHAM AWASYA <63654949+SubhamAwasya@users.noreply.github.com>
2023-09-04 18:38:06 +0530

    can send user data to server

--- src/components/form.jsx
+++ src/components/form.jsx
@@ -1,7 +1,43 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 import "./css/form.css";
+
 function Form() {
   const [isRegisterd, setIsRefisterd] = useState(false);
+  const [fName, setfName] = useState("");
+  const [lName, setlName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [cPassword, setcPassword] = useState("");
+
+  //User data that is send to the server///////////////////////////////////////////////
+  const logInData = {
+    email: email,
+    password: password,
+  };
+  const signUpData = {
+    fname: fName,
+    lname: lName,
+    email: email,
+    password: password,
+    confirmP: cPassword,
+  };
+  //////////////////////////////////////////////////////////////////////////////
+
+  async function LogIn(e) {
+    e.preventDefault();
+    await axios.post("http://localhost:3000/", logInData).then((res) => {
+      console.log(res.status);
+    });
+  }
+  //////////////////////////////////////////////////////////////////////////////
+
+  function SignUp(e) {
+    e.preventDefault();
+    axios.post("http://localhost:3000/", signUpData).then((res) => {
+      console.log(res.data);
+    });
+  }
 
   return (
     <>
@@ -11,6 +47,8 @@ function Form() {
         </h2>
         {isRegisterd && (
           <input
+            value={fName}
+            onChange={(e) => setfName(e.target.value)}
             className="finput"
             name="fname"
             placeholder="First Name"
@@ -20,6 +58,8 @@ function Form() {
         )}
         {isRegisterd && (
           <input
+            value={lName}
+            onChange={(e) => setlName(e.target.value)}
             className="finput"
             name="lname"
             placeholder="Last Name"
@@ -28,6 +68,8 @@ function Form() {
           ></input>
         )}
         <input
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
           className="finput"
           name="email"
           placeholder="Email"
@@ -35,6 +77,8 @@ function Form() {
           required
         ></input>
         <input
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
           className="finput"
           name="password"
           placeholder="Password"
@@ -43,6 +87,8 @@ function Form() {
         ></input>
         {isRegisterd && (
           <input
+            value={cPassword}
+            onChange={(e) => setcPassword(e.target.value)}
             className="finput"
             name="confirmpassword"
             placeholder="Confirm Password"
@@ -50,9 +96,12 @@ function Form() {
             required
           ></input>
         )}
-
-        <button className="sbtn" type="submit">
-          {isRegisterd ? "Sign up" : "Log in"}
+        <button
+          className="sbtn"
+          type="submit"
+          onClick={isRegisterd ? SignUp : LogIn}
+        >
+          {isRegisterd ? "SignUp" : "LogIn"}
         </button>
         <a
           onClick={() => {
